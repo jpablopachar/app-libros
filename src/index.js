af895ea91331a7d1c678e5dfeb9cd42dc320e748
Juan Pablo Pachar Viñan <jpablopachar1993@gmail.com>
2019-02-21 00:44:20 -0500

    Se inicia y se configura middelawares para el servidor

---
+++ src/index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const morgan = require('morgan');
+const multer = require('multer');
+
+const path = require('path');
+
+// require('./databases/database');
+
+const app = express();
+
+/*                  Ajustes                     */
+// Usa el puerto establecido o usa el puerto 3000
+app.set('port', process.env.PORT || 3000);
+
+/*                   Vistas                      */
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+/*             Archivos Estáticos                */
+app.use(express.static(path.join(__dirname, 'public')));
+
+/*                 Middleware                   */
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const storage = multer.diskStorage({
+  destination: path.join(__dirname, 'public/imagenes/uploads'),
+  filename: (req, file, cb) => {
+    cb(null, new Date().getTime() + path.extname(file.originalname));
+  },
+});
+
+app.use(multer({ storage }).single('imagen'));
+
+/*              Variables globales              */
+/*app.use((req, res, next) => {
+  app.locals.format = format;
+
+  next();
+});*/
+
+/*                    Rutas                      */
+// app.use(require('./routes/imagen'));
+
+// Escucha en el puerto establecido
+app.listen(app.get('port'), () => console.log(`Servidor en puerto ${app.get('port')}`));
