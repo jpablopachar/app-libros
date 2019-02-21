0f543f17024d1a154c4ba12b54975f6a68dd405b
Juan Pablo Pachar Viñan <jpablopachar1993@gmail.com>
2019-02-21 01:26:32 -0500

    Se crea las variables de entorno de la aplicacion y se mejora los entornos de desarrollo y produccion

--- server/database.js
+++ server/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/DBAppLibros', {
+mongoose.connect(process.env.MONGODB_URI, {
   useCreateIndex: true,
   useNewUrlParser: true,
   useFindAndModify: false,
